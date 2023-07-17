3755f40f3c3b7674a463f73ee63c03c71037d2fa
Jason <jasonyen1009@gmail.com>
2023-07-17 19:52:08 +0800

    Initial Commit

---
+++ SpeedTracker/SpeedTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTrackerApp.swift
+//  SpeedTracker
+//
+//  Created by Yen Hung Cheng on 2023/7/17.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeedTrackerUITests/SpeedTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpeedTrackerUITestsLaunchTests.swift
+//  SpeedTrackerUITests
+//
+//  Created by Yen Hung Cheng on 2023/7/17.
+//
+
+import XCTest
+
+final class SpeedTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
